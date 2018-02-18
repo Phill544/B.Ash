fcb24e0434ef4f2f07b9ac95c1eef26d1e96c6b3
Phill <philljm544@gmail.com>
2018-02-18 20:07:17 +1100

    Cleaned up ScreennGrab

--- Main Build/Scripts/ScreenGrab.py
+++ Main Build/Scripts/ScreenGrab.py
@@ -4,25 +4,14 @@ from PIL import ImageGrab # To take screenshots and save them to memory
 import cv2 # TO convert the colours correctly
 import numpy
 
-print(os.getcwd())
+###########################################################
+# This script is used to start, find, and get screenshots #
+# of the emulator window.                                 #
+###########################################################
 
 
 
-"""
-
-- Take Screenshot of window
-- Find tiles of importance inside of window
-- Run functions of tiles
-- Maintain and update data about position in game world, e.g. On route XX
-- Implement States that the bot will be in. i.e Currently training/returning to town/challenging next gym
-- Choose next state based on goals.
-- Able to identify and keep track of pokemon
-- Able to at identify and complete battle state
-- Able to use items
-- Able to input into the game to change state and complete goals
-
-
-"""
+# TODO: Move this function to a more accurate module.
 def StartEmulator():
     try:
         os.startfile(r"..\Emulator\VisualBoyAdvance-1.8.0-511.exe")
@@ -30,6 +19,7 @@ def StartEmulator():
     except:
         print('[-] Error loading emulator.')
 
+# Depreciated function, do not use.
 def ScreenShotWindow():
     window = win32gui.FindWindow("Afx:400000:0:0:900011:c8f0b35",None)
     rect = win32gui.GetClientRect(window)
@@ -38,10 +28,14 @@ def ScreenShotWindow():
     img = ImageGrab.grab(captureRect)    
     return img
 
+
+
 def FindWindow():
     window = win32gui.FindWindow(None,"VisualBoyAdvance")
     return window
 
+
+# Takes a screenshot of the window.
 def GetScreenShot(window):
     rect = win32gui.GetClientRect(window)
     xstart, ystart = win32gui.ClientToScreen(window,(0,0))
@@ -51,6 +45,8 @@ def GetScreenShot(window):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
 
+
+# Takes a snippit of the window starting at the xpos and ypos, and going for the height/width
 def GetScreenSnippit(window, xpos, ypos, width, height):
     rect = win32gui.GetClientRect(window)
     xstart, ystart = win32gui.ClientToScreen(window,(0,0))
@@ -60,6 +56,8 @@ def GetScreenSnippit(window, xpos, ypos, width, height):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
 
+
+# Depreciated function, do not use.
 def FindWithWinSpy(classID):
     window = win32gui.FindWindow(str(classID),None)
     return window
