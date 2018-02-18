2d3aff6c8a5aadf0eb4cb0e28fc4cb6e4e92bae8
Phill <philljm544@gmail.com>
2018-02-18 16:39:17 +1100

    Cleaned up Install_Modules

--- Main Build/Scripts/Install_Modules.py
+++ Main Build/Scripts/Install_Modules.py
@@ -1,5 +1,7 @@
 import pip
 
+# This Module is used to import the modules that B.Ash needs in order to function
+
 def install_Modules():
     pip.main(['install','pypiwin32'])
     pip.main(['install','numpy'])
